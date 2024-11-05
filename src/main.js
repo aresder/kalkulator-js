15b20c6399e83aef7bd1ee7a1800a8edf0bd2c73
aresder <anggariadisyahputra@gmail.com>
2024-11-06 00:00:47 +0700

    Menambahkan validasi error dan merapikan layout

--- src/main.js
+++ src/main.js
@@ -1,35 +1,58 @@
 let layar_hasil = document.querySelector('#layar_hasil')
+let input = ''
+
+const dinamisInput = (input) => {
+  layar_hasil.value = input
+
+  let maxFontSize = 40;
+  let minFontSize = 16;
+
+  layar_hasil.style.fontSize = maxFontSize + 'px';
+
+  while (layar_hasil.scrollWidth > layar_hasil.clientWidth && parseInt(layar_hasil.style.fontSize) > minFontSize) {
+    layar_hasil.style.fontSize = (parseInt(layar_hasil.style.fontSize) - 1) + 'px';
+  }
+}
 
 const tampilkanKeLayar = (value) => {
   let hasil = layar_hasil.value
+  if (hasil.includes(',')) input = hasil.replace(/,/g, '')
 
-  if (hasil.includes(',')) hasil = hasil.replace(/,/g, '')
-  if (hasil === '0' && value !== '0' && value !== '.') hasil = hasil.replace(/0/g, '')
-
-  return layar_hasil.value = hasil + value
+  input += value
+  dinamisInput(input)
 }
 
 const operasiAritmatika = () => {
-  if (layar_hasil.value.includes('^')) {
-    const pisahkan = layar_hasil.value.split('^')
-    const angka1 = Number(pisahkan[0])
-    const angka2 = Number(pisahkan[1])
-    const proses = Math.pow(angka1, angka2)
+  try {
+    if (layar_hasil.value.includes('^')) {
+      const pisahkan = layar_hasil.value.split('^')
+      const angka1 = Number(pisahkan[0])
+      const angka2 = Number(pisahkan[1])
+      const proses = Math.pow(angka1, angka2)
+      const format = proses.toLocaleString('en-EN')
+
+      return layar_hasil.value = format
+    }
+
+    const proses = eval(layar_hasil.value)
     const format = proses.toLocaleString('en-EN')
 
     return layar_hasil.value = format
+  } catch (err) {
+    console.error(err.message)
+    layar_hasil.value = 'err'
+    setTimeout(() => {
+      reset()
+    }, 1000)
   }
-
-  const proses = eval(layar_hasil.value)
-  const format = proses.toLocaleString('en-EN')
-
-  return layar_hasil.value = format
 }
 
 const spasiMundur = () => {
-  return layar_hasil.value = layar_hasil.value.slice(0, -1)
+  layar_hasil.value = layar_hasil.value.slice(0, -1)
 }
 
 const reset = () => {
-  return layar_hasil.value = '0'
+  layar_hasil.value = ''
+  input = ''
+  layar_hasil.style.removeProperty('font-size')
 }
